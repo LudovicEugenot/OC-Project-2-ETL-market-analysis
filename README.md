471dd5a8445ed99f39aa745c5e13b2e7e45bbac0
Aurelien lebray <aurelien.lebray@gmail.com>
2024-05-03 19:23:29 +0200

    Lesson 03/05/2024

---
+++ README.md
@@ -0,0 +1,9 @@
+Rajouter les instructions pour l'environnement virtuel ...
+
+Pour installer les dépendances
+
+```
+pip install -r requirements.txt
+```
+
+Pour lancer les différents scripts ...
--- first_step.py
+++ first_step.py
@@ -1,22 +1,39 @@
 import requests
 from bs4 import BeautifulSoup
+from csv import writer
 
-##book_url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-
-def scrap_book():
+# Extract
+def scrap_book(book_url):
     """
     Gets soup from himalayas url for now though "response".
     :return: Get soup back.
     """
-    response = requests.get("https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html")
-    # to get input later response = requests.get(input("Paste url of book you want to get the name of."))
+    response = requests.get(book_url)
     soup = BeautifulSoup(response.text, "html.parser")
     return soup
 
+# Transform
+def transform_book(book_soup):
+    book_title = book_soup.h1.text.strip()
+    table = book_soup.find('table', {"class": "table table-striped"})
+    rows = table.find_all('tr')
+    upc = rows[0].find('td').text
+    return [upc, book_title]
+
+# Load
+def load_book(book_data):
+    with open('first_step.csv', 'w', newline='') as csvfile:
+        csvwriter = writer(csvfile)
+        csvwriter.writerow(['universal_product_code (upc)', 'title']) # Write the header
+        csvwriter.writerow(book_data)
+        
+
 def main():
-    book_scraped = scrap_book()
-    book_title = book_scraped.title.text
-    print(book_title)
+    book_url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
+
+    book_scraped = scrap_book(book_url)
+    book_transformed = transform_book(book_scraped)
+    load_book(book_transformed)
 
 if __name__  == '__main__':
     main()
